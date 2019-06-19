cffcee4c2238f346f369b31939943c2efbc4f61f
git <min.zhang@handpay.com.cn>
2019-06-19 14:53:13 +0800

    Update preDBcreate.py

--- preDBcreate.py
+++ preDBcreate.py
@@ -49,6 +49,10 @@ DEFINE_string('target_user', 'root', 'Manage target database mysql account')
 DEFINE_string('source_pwd', 'null', 'Manage source database mysql password')
 DEFINE_string('target_pwd', 'null', 'Manage target database mysql password')
 
+DEFINE_string('source_dev', '/dev/mapper/vg_mysql-lv_mysql', 'source db dev.')
+DEFINE_string('snap_dev', '/dev/mapper/vg_mysql-mysqllvsnap', 'snap db dev.')
+
+
 DEFINE_string('db_host', '127.0.0.1', 'source and target database host ip address')
 
 
@@ -105,21 +109,21 @@ def stop_mysql(socket,user,pwd):
         logger.error(str(e))
     logger.info("stop mysql intance end.")
 
-def umount_dev(name,s1):
-    logger.info("umount %s .") % s1
-    os_cmd1 = "fuser -k %s" % name
-    os_cmd2 = "umount %s" % name
+def umount_dev(group_name):
+    logger.info("umount %s .") % group_name
+    os_cmd1 = "fuser -k %s" % group_name
+    os_cmd2 = "umount %s" % group_name
     try:
         for i in os_cmd1,os_cmd2:
             result = os.popen(i).readlines()
-            print(result)
+            logger.info(result)
     except Exception as e:
-        logger.info(str(e))
-    logger.info("umount %s end.") % s1
+        logger.error(str(e))
+    logger.info("umount %s end.") % group_name
     
-def create_snap(name):
+def create_snap(size,group_name):
     logger.info("create snap mysqllvsnap") 
-    os_cmd = "lvcreate -L 200G -s -n mysqllvsnap %s" % name
+    os_cmd = "lvcreate -L %sG -s -n mysqllvsnap %s" % (int(size),group_name)
     
     try:
         result = os.popen(os_cmd).readlines()
@@ -138,19 +142,19 @@ def start_mysql(cfg ,s1):
         logger.info(str(e))
     logger.info("starting %s mysql db end.") % s1
 
-def mount_snap_dev(name,s1):
-    logger.info("mount %s .") % s1
-    os_cmd = "mount %s /snap_data/" % name
+def mount_dev(group_name,dir_name):
+    logger.info("mount %s .") % group_name
+    os_cmd = "mount %s /%s/" % (group_name,dir_name)
     try:
         result = os.popen(os_cmd).readlrebooines()
         print(result)
     except Exception as e:
         logger.info(str(e))
-    logger.info("mount %s end.") % s1
+    logger.info("mount %s end.") % group_name
 
 def remove_snap_dev():
     logger.info("remove snap ")
-    os_cmd = "lvremove /dev/mapper/vg_mysql-mysqllvsnap"
+    os_cmd = "lvremove %s" % FLAGS.snap_dev
     try:
         result = os.popen(os_cmd).readlines()
         print(result)
@@ -159,7 +163,7 @@ def remove_snap_dev():
     logger("remove snap end.")
     
 def main():
-    #关闭source db
+    #close source db and slave thread
     #mysql.sock   
     #mysql3307.sock
     logger.info("close source db.")
@@ -169,6 +173,34 @@ def main():
     
     logger.info("finished close source db.")
     
+    #source_dev snap_dev
+    umount_dev(FLAGS.source_dev)
+    
+    #remove history snap dev
+
+    lvm_snap="lvs |grep 'mysqllvsnap' |wc -l"
+    if lvm_snap == 0:
+        logger.info("Not snapshot group, you don't remove snapshot group.")
+    else:
+        logger.info("close target db.")
+        stop_mysql('mysql3307',FLAGS.target_user,FLAGS.target_pwd)
+        logger.info("finished close target db instance.")
+        
+        #
+        #
+        #
+        umount_dev(FLAGS.snap_dev)
+        remove_snap_dev()
+    
+    #create snap dev and mount 
+    #/dev/mapper/vg_mysql-lv_mysql  /dev/mapper/vg_mysql-mysqllvsnap
+    #
+    create_snap(1,FLAGS.snap_dev)
+    mount_dev(FLAGS.snap_dev,"snap_data")
+    
+    mount_dev(FLAGS.source_dev,"data")
+    
+        
     
 
 if __name__ == '__main__':
