aa55c501b92583dd1dc0f557bfc337c3922ce624
git <min.zhang@handpay.com.cn>
2019-06-25 15:56:55 +0800

    Update replCheckStatus.py

--- replCheckStatus.py
+++ replCheckStatus.py
@@ -151,7 +151,7 @@ class singleReplCheck(object):
        
         do_getlog = GET_FROM_LOG % (com_mysqlbinlog, r['Master_Host'], int(r['Master_Port']),FLAGS.user,FLAGS.password, int(log_start_position), int(log_stop_position),log_file_name)
         do_getlog2 = GET_FROM_LOG2 % (com_mysqlbinlog, r['Master_Host'], int(r['Master_Port']),FLAGS.user,FLAGS.password, int(log_start_position), int(log_stop_position),log_file_name,pk_seq)
-       
+        '''       
         c=os.popen(gfld_c).readlines()[0]
         print('c: %s' % c)
         if (c > '1'): 
@@ -160,7 +160,8 @@ class singleReplCheck(object):
             pk_value=1
         else:
             print('opt 1 row 1032')
-            pk_value = os.popen(do_getlog2).readlines()[0].split("=",2)[1].rstrip() 
+        '''
+        pk_value = os.popen(do_getlog2).readlines()[0].split("=",2)[1].rstrip() 
         
         print ("pk_value : %s" % pk_value)
         sql = repairSql_1032(db_table, pk_value, pk_seq)
