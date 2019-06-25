b88398a4cbb5abd3f0f22eb6a83cba34a2d103e7
git <min.zhang@handpay.com.cn>
2019-06-25 14:13:11 +0800

    Update replCheckStatus.py

--- replCheckStatus.py
+++ replCheckStatus.py
@@ -2,14 +2,20 @@
 
 
 """
-1.无法修复一个事务中有多个DML语句的修复
+限制条件：
+1.无法修复一个事务中有多个DML语句导致的1032和1062
+2.无法修复复合主键导致的1032和1062
+3.无在修复只读slave上1032和1062
+4.无法修复并行复制slave上1032和1062
+5.无法修复
+
 """
 
 import sys
 import os
 import re
 import optparse
-import pymysql,commands
+import pymysql
 import platform
 from util.record_logging import RecordLog
 
@@ -23,8 +29,9 @@ com_mysqlbinlog = "/usr/local/mysql/bin/mysqlbinlog"
 r1062 = r"Could not execute Write_rows event on table (.*); Duplicate entry '(.*)' for key 'PRIMARY', Error_code: 1062; handler error HA_ERR_FOUND_DUPP_KEY; the event's master log (.*), end_log_pos (\d+)"
 u1032 = r"Could not execute (.*)_rows event on table (.*); Can't find record in (.*), Error_code: 1032; handler error HA_ERR_KEY_NOT_FOUND; the event's master log (.*), end_log_pos (\d+)"
 
-GET_FROM_LOG="%s -v --base64-output=decode-rows -R --host='%s' --port=%d --user='%s' --password='%s' --start-position=%d --stop-position=%d %s |grep @%s|head -n 1"
-
+#GET_FROM_LOG="%s -v --base64-output=decode-rows -R --host='%s' --port=%d --user='%s' --password='%s' --start-position=%d --stop-position=%d %s |grep @%s|head -n 1"
+GET_FROM_LOG="%s -v --base64-output=decode-rows -R --host='%s' --port=%d --user='%s' --password='%s' --start-position=%d --stop-position=%d %s |egrep '###'"
+GET_FROM_LOG_DML_COUNT="%s -v --base64-output=decode-rows -R --host='%s' --port=%d --user='%s' --password='%s' --start-position=%d --stop-position=%d %s |egrep '###' |wc -l"
 
 FLAGS = optparse.Values()
 parser = optparse.OptionParser()
@@ -128,8 +135,6 @@ class singleReplCheck(object):
         
     @staticmethod
     def handler_1032(r, rpl):
-        #u1032 = r"Could not execute (.*)_rows event on table (.*); Can't find record in (.*), Error_code: 1032; handler error HA_ERR_KEY_NOT_FOUND; the event's master log (.*), end_log_pos (\d+)"
-        print (r['Last_SQL_Error'])
         p = re.compile(u1032)
         m = p.search(r['Last_SQL_Error'])
         db_table = m.group(2)
@@ -139,13 +144,17 @@ class singleReplCheck(object):
         log_start_position = r['Exec_Master_Log_Pos']
         pk_seq = get_tb_pk(db_table)[1]   
         print ("pk_seq : %s" % pk_seq)
-        do_getlog = GET_FROM_LOG % (com_mysqlbinlog, r['Master_Host'], int(r['Master_Port']),FLAGS.user,FLAGS.password, int(log_start_position), int(log_stop_position),  log_file_name,pk_seq)
+        gfld_c = GET_FROM_LOG_DML_COUNT % (com_mysqlbinlog, r['Master_Host'], int(r['Master_Port']),FLAGS.user,FLAGS.password, int(log_start_position), int(log_stop_position),log_file_name,pk_seq)
+       
+        do_getlog = GET_FROM_LOG % (com_mysqlbinlog, r['Master_Host'], int(r['Master_Port']),FLAGS.user,FLAGS.password, int(log_start_position), int(log_stop_position),log_file_name,pk_seq)
        
         #pk_value = os.popen(do_getlog).readlines()[0].split("=",2)[1].rstrip()
-       
-        (status,output)=commands.getstatusoutput(do_getlog)
-        print(status)
-        print(output)
+        c=os.popen(gfld_c).readlines()[0]
+        print(c)
+        p=os.popen(do_getlog).readlines()[0]
+        print(p)
+        
+        
         
         pk_value=1
         print ("pk_value : %s" % pk_value)
@@ -172,7 +181,6 @@ def repairSql_1032(db_table, pk_value, pk_seq):
     r = "replace into %s.%s "%(db,tb_name)
     
     sql = "select column_name, ORDINAL_POSITION from information_schema.columns where table_schema='%s' and table_name='%s' and IS_NULLABLE='NO';" % (db, tb_name)
-    #print sql
     
     col_list=''
     value_list=''
