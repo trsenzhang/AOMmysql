59d035a7555003109c7421b939b841fedbeb1759
git <min.zhang@handpay.com.cn>
2019-07-01 17:23:48 +0800

    Update test1032.py

--- test1032.py
+++ test1032.py
@@ -220,7 +220,6 @@ class singleReplCheck(object):
         
     @staticmethod
     def handler_1032(r, rpl):
-        global binlog_result
         err_msg = r['Last_SQL_Error']
         col_info=[]
         event = err_msg.split('event')[0].split('execute')[1].strip()
@@ -238,6 +237,7 @@ class singleReplCheck(object):
         print(do_getlog2)
         #isn't multi DML in the transaction
         for line in do_getlog2:
+            binlog_result=0
             if line.startswith('#') and re.search("flags: STMT_END_F", line):
                 print("have SMTM_END_F,is ok.")
                 binlog_result=os.popen(do_getlog2).readlines()
