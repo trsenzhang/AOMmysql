945766aa84df36f05b5348a7aa1f61789ba0d741
git <min.zhang@handpay.com.cn>
2019-07-02 11:41:31 +0800

    Update test1032.py

--- test1032.py
+++ test1032.py
@@ -155,7 +155,7 @@ def find_row_recode_from_binlog(event, table_name, result):
             event_flag = 1
         if line.startswith(option_keyword):
             recode_list.append('---line---')
-            recode_list.append(option_keyword+new_table_name)
+            recode_list.append(option_keyword+' '+new_table_name)
             option_flag = 1
         if re.search('WHERE', line):
             where_flag = 1
